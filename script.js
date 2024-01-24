7071d50fd68d425459573ea86a8e738df2a9d858
franzsaro <cacayanfranzsaro@gmail.com>
2024-01-24 15:41:39 +0800

    Resume v.3
    Added sticky effect on navbar

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+window.onscroll = function() {stickyNav(), stickyHamburgerNav()};
+
+var navbar = document.getElementById("desktop-nav");
+var hamburgerNav = document.getElementById("hamburger-nav");
+var sticky = navbar.offsetTop;
+var hamburgerStick = hamburgerNav.offsetTop;
+
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    }
+    else {
+        navbar.classList.remove("sticky")
+    }
+}
+
+function stickyHamburgerNav() {
+    if (window.pageYOffset >= hamburgerStick) {
+        hamburgerNav.classList.add("sticky")
+    }
+    else {
+        hamburgerNav.classList.remove("sticky")
+    }
+}
